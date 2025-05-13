fb7364155cba5627eab3fbea04c9459b42afab2b
JuanJo Arenas <dragonex579@gmail.com>
2025-05-13 12:35:48 -0500

    Configuracion models P1
    se configura el fillable de cada modelo

--- orderweb/app/Models/Order.php
+++ orderweb/app/Models/Order.php
@@ -5,15 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Technician extends Model
+class Order extends Model
 {
     use HasFactory;
 
-    protected $table = 'technician';
+    protected $table = 'order';
     protected $fillable = [
-        'document',
-        'name',
-        'speciality',
-        'phone'
+        'legalization_date',
+        'address',
+        'city',
+        'observation_id',
+        'causal_id'
     ];
 }
