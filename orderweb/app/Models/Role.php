7ef89757082bbce63a946905b53f627966cd602a
JuanJo Arenas <dragonex579@gmail.com>
2025-05-15 11:19:52 -0500

    Modelos P2 y vistas basicas
    se termina configuracion de models y creacion de views basicas

--- orderweb/app/Models/Role.php
+++ orderweb/app/Models/Role.php
@@ -13,5 +13,9 @@ class Role extends Model
     protected $fillable = [
         'name'
     ];
-    
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }
---
+++ orderweb/resources/views/test2.blade.php
@@ -0,0 +1,14 @@
+@extends('templates.base')
+@section('title', 'Test 2')
+@section('content')
+
+        <h1>Test 2</h1>
+        <q>No soy un hombre de plegarias, pero si estas en el cielo, ayudame Superman!</q>
+        <small>Homero J. Simpson</small>
+        <button onclick="show_alert()">Click!</button>
+
+@endsection
+
+@section('scripts')
+        <script src="{{ asset('js/test.js') }}"></script>
+@endsection
