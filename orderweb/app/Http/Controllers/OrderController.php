bf422a554a15f5e6b32314d39a791d6275978acd
JuanJo Arenas <dragonex579@gmail.com>
2025-05-27 11:21:28 -0500

    Order P2
    se termina el modulo order y se cambia la vista principal a index

--- orderweb/app/Http/Controllers/OrderController.php
+++ orderweb/app/Http/Controllers/OrderController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Activity;
 use App\Models\Causal;
 use App\Models\Observation;
 use App\Models\Order;
+use Illuminate\Support\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
@@ -62,7 +65,18 @@ class OrderController extends Controller
                 ['name' => 'BUGA', 'value' => 'BUGA'],
                 ['name' => 'PALMIRA', 'value' => 'PALMIRA']
             ];
-            return view('order.edit', compact('order', 'causals', 'observations', 'cities'));
+
+            //consultar actividades disponibles
+            $query = DB::select("SELECT * FROM activity WHERE activity.id NOT IN (
+                                SELECT order_activity.activity_id FROM order_activity
+                                WHERE order_activity.order_id = ?)", [$id]);
+
+            $availableActivities = Collection::make($query);
+
+            //consultar actividades agrupadas a la orden
+            $addedActivities = $order->activities;
+
+            return view('order.edit', compact('order', 'causals', 'observations', 'cities', 'availableActivities', 'addedActivities'));
         }
         else
         {
@@ -105,5 +119,53 @@ class OrderController extends Controller
             session()->flash('warning', 'No se encuentra el registro solicitado');
         }
         return redirect()->route('order.index');
+    }
+
+    /**
+     * Add an activity to an order
+     */
+    public function add_activity(string $order_id, string $activity_id)
+    {
+        $order = Order::find($order_id);
+        if(!$order)
+        {
+            session()->flash('error', 'No se encuentra la orden');
+            return redirect()->route('order.edit', $order_id)->withInput();
+        }
+        $activity = Activity::find($activity_id);
+        if(!$activity)
+        {
+            session()->flash('error', 'No se encuentra la orden');
+            return redirect()->route('order.edit', $activity_id)->withInput();
+        }
+
+        //delete the activity in order_activity
+        $order->activities()->attach($activity_id);
+        session()->flash('message', 'Actividad agregada exitosamante');
+        return redirect()->route('order.edit', $order_id);
+    }
+
+    /**
+     * Remove an activity to an order
+     */
+    public function remove_activity(string $order_id, string $activity_id)
+    {
+        $order = Order::find($order_id);
+        if(!$order)
+        {
+            session()->flash('error', 'No se encuentra la orden');
+            return redirect()->route('order.edit', $order_id)->withInput();
+        }
+        $activity = Activity::find($activity_id);
+        if(!$activity)
+        {
+            session()->flash('error', 'No se encuentra la orden');
+            return redirect()->route('order.edit', $activity_id)->withInput();
+        }
+
+        //delete the activity in order-activity
+        $order->activities()->detach($activity_id);
+        session()->flash('message', 'Actividad removida exitosamante');
+        return redirect()->route('order.edit', $order_id);
     }
 }
--- orderweb/resources/views/order/edit.blade.php
+++ orderweb/resources/views/order/edit.blade.php
@@ -2,6 +2,7 @@
 @section('title', 'Editar Ordenes')
 @section('header', 'Editar Ordenes')
 @section('content')
+@include('templates/messages')
     <div class="row">
         <div class="col-lg-12 mb-4">
             <form action="{{ route('order.update', $order['id']) }}" method="POST">
@@ -69,24 +70,84 @@
 
             <div class="row">
                 <div class="col-lg-12 mb-4">
-                    <div class="card-shadow mb-4">
+                    <div class="card shadow mb-4">
                         <div class="card-header">
                             <h6 class="font-weight-bold text-primary m-0">Añadir/Retirar actividades</h6>
                         </div>
                         <div class="card-body">
-                            <div class="row-form-group">
+                            <div class="row form-group">
                                 <div class="col-lg-6">
                                     <label for="table_data">Actividades disponibles</label>
+                                    <table id="table_data" class="table table-striped table-hover">
+                                        <thead>
+                                            <th>Id</th>
+                                            <th>Descripcion</th>
+                                            <th>Horas</th>
+                                            <th>Agregar</th>
+                                        </thead>
+                                        <tbody>
+                                            @if(count($availableActivities) == 0)
+                                                <tr>
+                                                    <td colspan="4">
+                                                        No existen actividades disponibles
+                                                    </td>
+                                                </tr>
+                                            @else
+                                                @foreach ($availableActivities as $activity)
+                                                    <tr>
+                                                        <td>{{ $activity->id }}</td>
+                                                        <td>{{ $activity->description }}</td>
+                                                        <td>{{ $activity->hours }}</td>
+                                                        <td>
+                                                            <a href="{{ route('order.add_activity', [$order['id'], $activity->id]) }}" class="btn btn-success btn-circle btn-sm" title="Agregar">
+                                                                <i class="fas fa-fw fa-plus"></i>
+                                                            </a>
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                            @endif
+                                        </tbody>
+                                    </table>
                                 </div>
-                            </div>
                                 <div class="col-lg-6">
                                     <label for="table_data">Actividades agregadas</label>
+                                    <table id="table_data" class="table table-striped table-hover">
+                                        <thead>
+                                            <th>Id</th>
+                                            <th>Descripcion</th>
+                                            <th>Horas</th>
+                                            <th>Retirar</th>
+                                        </thead>
+                                        <tbody>
+                                            @if(count($addedActivities) == 0)
+                                                <tr>
+                                                    <td colspan="4">
+                                                        No existen actividades agregadas
+                                                    </td>
+                                                </tr>
+                                            @else
+                                                @foreach ($addedActivities as $activity)
+                                                    <tr>
+                                                        <td>{{ $activity->id }}</td>
+                                                        <td>{{ $activity->description }}</td>
+                                                        <td>{{ $activity->hours }}</td>
+                                                        <td>
+                                                            <a href="{{ route('order.remove_activity', [$order['id'], $activity->id]) }}" class="btn btn-danger btn-circle btn-sm" title="Agregar">
+                                                                <i class="fas fa-fw fa-minus"></i>
+                                                            </a>
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                            @endif
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
+        
         </div>
     </div>
 
